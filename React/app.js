254d0134ab7e3b431378346b2f646ad7832a6dad
Selahattin Demir <87477482+Selahaddin64@users.noreply.github.com>
2021-11-10 17:49:59 +0300

    Add files via upload

---
+++ React/app.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+async function getData(userId){
+    const {data:user}= await axios("https://jsonplaceholder.typicode.com/users/" + userId);
+
+    const { data: posts } = await axios(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`);
+
+    console.log(user);
+    console.log("posts:", posts);
+}
+export default getData;
+
